6a588066b01ed2b908836fbf11526ac832b3d28c
Gospel011 <ugochukwugospeli111@gmail.com>
2024-01-24 13:19:36 +0100

    Finished drawer

--- l_earn/lib/Presentation/Pages/home_page.dart
+++ l_earn/lib/Presentation/Pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:l_earn/BusinessLogic/AuthCubit/auth/auth_cubit.dart';
-import 'package:l_earn/Presentation/components/my_elevated_button.dart';
-import 'package:l_earn/Presentation/components/my_text_button.dart';
+import 'package:l_earn/Presentation/components/my_drawer.dart';
+
 import 'package:l_earn/utils/mixins.dart';
 
 class HomePage extends StatelessWidget with AppBarMixin {
@@ -20,52 +20,10 @@ class HomePage extends StatelessWidget with AppBarMixin {
                 icon: const Icon(Icons.menu_rounded));
           })
         ]),
-        endDrawer: Drawer(
-          child: Column(
-            children: [
-              //? Image
-              const DrawerHeader(
-                  decoration: BoxDecoration(shape: BoxShape.circle),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.blueGrey,
-                    radius: 48,
-                  )),
-
-              //? Logout
-              Row(
-                children: [
-                  MyTextButton(
-                      text: "Logout",
-                      onPressed: () {
-                        //TODO --> HANDLE LOGOUT
-                        print("Logout button pressed");
-                      }),
-                ],
-              ),
-
-              //? Contact us
-              Align(
-                alignment: Alignment.center,
-                child: InkWell(
-                  onTap: () {
-                    print('Contact us button pressed');
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.black,
-                        boxShadow: [
-                          BoxShadow(color: Colors.black.withOpacity(0.25), offset: Offset(4, 4))
-                        ],
-                        borderRadius: BorderRadius.circular(12)),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Contact us", style: TextStyle(color: Colors.white),),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+        endDrawer: BlocBuilder<AuthCubit, AuthState>(
+          builder: (context, state) {
+            return MyDrawer(user: state.user!);
+          }
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -78,3 +36,4 @@ class HomePage extends StatelessWidget with AppBarMixin {
         ));
   }
 }
+
---
+++ l_earn/lib/Presentation/components/my_drawer.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:l_earn/DataLayer/Models/user_model.dart';
+
+class MyDrawer extends StatelessWidget {
+  final User user;
+  const MyDrawer({
+    super.key,
+    required this.user
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          //? Image
+          DrawerHeader(
+              decoration: const BoxDecoration(shape: BoxShape.circle),
+              child: CircleAvatar(
+                backgroundColor: Colors.blueGrey,
+                foregroundImage: user.profilePicture != null ? NetworkImage(user.profilePicture!) : null,
+                radius: 48,
+              )),
+
+          //? Logout
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(12),
+              onTap: () {
+                //TODO --> HANDLE LOGOUT
+                print("Logout button pressed");
+              },
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "Logout",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          //? Contact us
+          GestureDetector(
+            // splashColor: Colors.white,
+            onTap: () {
+              print('Contact us button pressed');
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade500,
+                        offset: const Offset(1, 1),
+                        spreadRadius: 1,
+                        blurRadius: 5),
+                    BoxShadow(
+                        color: Colors.grey.shade300,
+                        offset: const Offset(-1, -1),
+                        spreadRadius: 1,
+                        blurRadius: 5),
+                  ],
+                  borderRadius: BorderRadius.circular(12)),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Contact us",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
